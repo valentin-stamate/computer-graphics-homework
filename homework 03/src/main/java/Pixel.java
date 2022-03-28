8104011d71c75ecabbff29d6f1d3562939e76118
Stamate Valentin <stamatevalentin125@gmail.com>
2022-03-28 10:02:55 +0300

    small change

--- homework 03/src/main/java/Pixel.java
+++ homework 03/src/main/java/Pixel.java
@@ -2,9 +2,12 @@ import processing.core.PApplet;
 
 public record Pixel(PApplet pApplet, int x, int y, int size) {
     public void draw() {
-        pApplet.stroke(20);
-
+        pApplet.stroke(200);
         pApplet.line(x, y - size / 2.0f, x, y + size / 2.0f);
         pApplet.line(x - size / 2.0f, y, x + size / 2.0f, y);
+
+        pApplet.noFill();
+        pApplet.stroke(80);
+        pApplet.rect(x - size / 2.0f, y - size / 2.0f, size, size);
     }
 }
