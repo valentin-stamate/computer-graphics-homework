ebe4609ed39be4c388bca6e8506a548f2813813c
Stamate Valentin <stamatevalentin125@gmail.com>
2022-04-19 10:09:58 +0300

    exercise 2

--- homework 06/src/main/java/MainClass.java
+++ homework 06/src/main/java/MainClass.java
@@ -3,7 +3,7 @@ import processing.core.PApplet;
 public class MainClass extends PApplet {
 
     public void settings() {
-        size(630, 630, P2D);
+        size(630, 630, P3D);
     }
 
     public void setup() {
@@ -13,15 +13,69 @@ public class MainClass extends PApplet {
         surface.setTitle("Homework 6");
     }
 
+    float r = 0;
+    float angle = ((float) Math.PI) / 180f;
+
     @Override
     public void draw() {
         background(20);
+        rectMode(CENTER);
+
+        noFill();
+        stroke(255);
+
+        playTriangle();
+    }
+
+    public void playRectangle() {
+        translate(width / 2f, height / 2f, 0);
+        rotateX(r);
+        rotateY(r);
+        rotateZ(r);
+        rect(0,0, 100,100);
+
+        r += angle;
+
+        rotateX(0);
+        rotateY(0);
+        rotateZ(0);
+    }
+
+    float zPosition = -1000;
+    float zAngle = 180 * angle;
+    float yAngle = 180 * angle;
+    float xAngle = 180 * angle;
+
+    public void playTriangle() {
+        translate(width / 2f, height / 2f, zPosition);
+        rotateX(xAngle);
+        rotateY(yAngle);
+        rotateZ(zAngle);
+        triangle(0, -66, 50, 33, -50, 33);
+
+        if (xAngle > 0f) {
+            xAngle -= 1.8 * angle;
+        }
+
+        if (yAngle > 0f) {
+            yAngle -= 1.8 * angle;
+        }
+
+        if (zAngle > 0f) {
+            zAngle -= 1.8 * angle;
+        }
+
+        if (zPosition < 0) {
+            zPosition += 10f;
+        }
+
+        rotateX(0);
+        rotateY(0);
+        rotateZ(0);
     }
 
     @Override
-    public void stop() {
-
-    }
+    public void stop() {}
 
     @Override
     public void mouseReleased() {
