0ab0ef41c8ec8a9b4e38292f9e1dcd462ef7963a
Stamate Valentin <stamatevalentin125@gmail.com>
2022-04-20 10:38:23 +0300

    scaling

--- homework 04/src/main/java/MainClass.java
+++ homework 04/src/main/java/MainClass.java
@@ -6,9 +6,18 @@ public class MainClass extends PApplet {
     private final int canvasWidth = 630;
     private final int canvasHeight = 630;
 
+    private final int gridRows = 30;
+    private final int gridColumns = 30;
+
+    float ratioWidth;
+    float ratioHeight;
 
     public MainClass() {
-        this.cartesianGrid = new CartesianGrid(this, canvasWidth, canvasHeight, 30, 30);
+        this.cartesianGrid = new CartesianGrid(this, canvasWidth, canvasHeight, gridRows, gridColumns);
+
+        this.ratioWidth = 1f * canvasWidth / gridColumns;
+        this.ratioHeight = 1f * canvasHeight / gridRows;
+
     }
 
     public void settings() {
@@ -26,9 +35,21 @@ public class MainClass extends PApplet {
     @Override
     public void draw() {
         background(255);
+
+        float currentRatioWidth = 1f * width / gridColumns;
+        float currentRationHeight = 1f * height / gridRows;
+
+        float scaleX = currentRatioWidth / ratioWidth;
+        float scaleY = currentRationHeight / ratioHeight;
+
+        pushMatrix();
+        scale(scaleX, scaleY);
+
         cartesianGrid.draw();
         cartesianGrid.drawCircle(0, 0, 20);
 //        cartesianGrid.fillEllipse(14, 14, 10, 14);
+
+        popMatrix();
     }
 
     @Override
